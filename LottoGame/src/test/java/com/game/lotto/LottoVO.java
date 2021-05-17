54c057d6cf36910398c4f51a95ad80e2937f1230
YoungDaLee <ldy0956@gmail.com>
2021-05-18 00:45:47 +0900

    spring::db연동

--- LottoGame/src/test/java/com/game/lotto/LottoVO.java
+++ LottoGame/src/test/java/com/game/lotto/LottoVO.java
@@ -2,122 +2,100 @@ package com.game.lotto;
 
 public class LottoVO {
  
-    private int cnt;                //회차
-    private String WINNER_1;            //1등 당첨자
-    private String WIN_MONEY_1;            //1등 당첨금액
-    private String WINNER_2;            //2등 당첨자
-    private String WIN_MONEY_2;            //1등 당첨금액
-    private String WINNER_3;            //3등 당첨자
-    private String WIN_MONEY_3;            //1등 당첨금액
-    private String WINNER_4;            //4등 당첨자
-    private String WIN_MONEY_4;            //1등 당첨금액
-    private String WINNER_5;            //5등 당첨자
-    private String WIN_MONEY_5;            //1등 당첨금액
-    private String ROTO_NUM1;            //당첨번호1
-    private String ROTO_NUM2;            //당첨번호2
-    private String ROTO_NUM3;            //당첨번호3
-    private String ROTO_NUM4;            //당첨번호4
-    private String ROTO_NUM5;            //당첨번호5
-    private String ROTO_NUMB;            //보너스번호
+    private int cnt;            //회차
+    private int num1;           //당첨번호1
+    private int num2;           //당첨번호2
+    private int num3;           //당첨번호3
+    private int num4;           //당첨번호4
+    private int num5;           //당첨번호5
+    private int num6;           //당첨번호6
+    private int bonus;          //보너스번호
+    private long price1;        //1등당첨금
+    private long price2;        //2등당첨금
+    private int price3;         //3등당첨금
+    private int price4;         //4등당첨금
+    private int price5;         //5등당첨금
     
 
-    public String getWINNER_1() {
-        return WINNER_1;
-    }
-    public void setWINNER_1(String wINNER_1) {
-        WINNER_1 = wINNER_1;
-    }
-    public String getWIN_MONEY_1() {
-        return WIN_MONEY_1;
-    }
-    public void setWIN_MONEY_1(String wIN_MONEY_1) {
-        WIN_MONEY_1 = wIN_MONEY_1;
-    }
-    public String getWINNER_2() {
-        return WINNER_2;
-    }
-    public void setWINNER_2(String wINNER_2) {
-        WINNER_2 = wINNER_2;
-    }
-    public String getWIN_MONEY_2() {
-        return WIN_MONEY_2;
-    }
-    public void setWIN_MONEY_2(String wIN_MONEY_2) {
-        WIN_MONEY_2 = wIN_MONEY_2;
-    }
-    public String getWINNER_3() {
-        return WINNER_3;
-    }
-    public void setWINNER_3(String wINNER_3) {
-        WINNER_3 = wINNER_3;
-    }
-    public String getWIN_MONEY_3() {
-        return WIN_MONEY_3;
-    }
-    public void setWIN_MONEY_3(String wIN_MONEY_3) {
-        WIN_MONEY_3 = wIN_MONEY_3;
-    }
-    public String getWINNER_4() {
-        return WINNER_4;
-    }
-    public void setWINNER_4(String wINNER_4) {
-        WINNER_4 = wINNER_4;
-    }
-    public String getWIN_MONEY_4() {
-        return WIN_MONEY_4;
-    }
-    public void setWIN_MONEY_4(String wIN_MONEY_4) {
-        WIN_MONEY_4 = wIN_MONEY_4;
-    }
-    public String getWINNER_5() {
-        return WINNER_5;
-    }
-    public void setWINNER_5(String wINNER_5) {
-        WINNER_5 = wINNER_5;
-    }
-    public String getWIN_MONEY_5() {
-        return WIN_MONEY_5;
-    }
-    public void setWIN_MONEY_5(String wIN_MONEY_5) {
-        WIN_MONEY_5 = wIN_MONEY_5;
-    }
-    public String getROTO_NUM1() {
-        return ROTO_NUM1;
-    }
-    public void setROTO_NUM1(String rOTO_NUM1) {
-        ROTO_NUM1 = rOTO_NUM1;
-    }
-    public String getROTO_NUM2() {
-        return ROTO_NUM2;
-    }
-    public void setROTO_NUM2(String rOTO_NUM2) {
-        ROTO_NUM2 = rOTO_NUM2;
-    }
-    public String getROTO_NUM3() {
-        return ROTO_NUM3;
-    }
-    public void setROTO_NUM3(String rOTO_NUM3) {
-        ROTO_NUM3 = rOTO_NUM3;
-    }
-    public String getROTO_NUM4() {
-        return ROTO_NUM4;
-    }
-    public void setROTO_NUM4(String rOTO_NUM4) {
-        ROTO_NUM4 = rOTO_NUM4;
-    }
-    public String getROTO_NUM5() {
-        return ROTO_NUM5;
-    }
-    public void setROTO_NUM5(String rOTO_NUM5) {
-        ROTO_NUM5 = rOTO_NUM5;
-    }
-    public String getROTO_NUMB() {
-        return ROTO_NUMB;
-    }
-    public void setROTO_NUMB(String rOTO_NUMB) {
-        ROTO_NUMB = rOTO_NUMB;
-    }
- 
     
+    public int getCnt() {
+    	return cnt;
+    }
+    public void setCnt(int Cnt) {
+    	cnt = Cnt;
+    }
+    public int getNum1() {
+    	return num1;
+    }
+    public void setNum1(int Num1) {
+    	num1 = Num1;
+    }
+    public int getNum2() {
+    	return num2;
+    }
+    public void setNum2(int Num2) {
+    	num2 = Num2;
+    }
+    public int getNum3() {
+    	return num3;
+    }
+    public void setNum3(int Num3) {
+    	num3 = Num3;
+    }
+    public int getNum4() {
+    	return num4;
+    }
+    public void setNum4(int Num4) {
+    	num1 = Num4;
+    }
+    public int getNum5() {
+    	return num5;
+    }
+    public void setNum5(int Num5) {
+    	num5 = Num5;
+    }
+    public int getNum6() {
+    	return num6;
+    }
+    public void setNum6(int Num6) {
+    	num6 = Num6;
+    }
+
+    public int getBonus() {
+    	return bonus;
+    }
+    public void setBonus(int BnusNo) {
+    	bonus = BnusNo;
+    }
+    public long getPrice1() {
+    	return price1;
+    }
+    public void setPrice1(long Price1) {
+    	price1 = Price1;
+    }
+    public long getPrice2() {
+    	return price2;
+    }
+    public void setPrice2(long Price2) {
+    	price2 = Price2;
+    }
+    public int getPrice3() {
+    	return price3;
+    }
+    public void setPrice3(int Price3) {
+    	price3 = Price3;
+    }
+    public int getPrice4() {
+    	return price4;
+    }
+    public void setPrice4(int Price4) {
+    	price4 = Price4;
+    }
+    public int getPrice5() {
+    	return price5;
+    }
+    public void setPrice5(int Price5) {
+    	price5 = Price5;
+    }
     
 }
